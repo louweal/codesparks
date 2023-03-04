adb09a3c627f1741368d7ebff11e4c325c98da7f
louweal <anneloeslouwe@live.nl>
2023-03-04 13:52:22 +0100

    changes background

--- components/SparkleOld.vue
+++ components/SparkleOld.vue
@@ -1,12 +1,12 @@
 <template>
-  <img
+  <div
     class="sparkle"
-    src="@/images/hbar.svg"
     :style="{
+      background: randomGradient,
       left: randomXPos,
       top: randomYPos,
       width: randomSize,
-      height: 1.0774 * randomSize,
+      height: randomSize,
       animationDelay: randomDelay,
     }"
   />
@@ -28,11 +28,11 @@ export default {
   },
 
   computed: {
-    // randomGradient() {
-    //   return `radial-gradient( circle, ${this.randomRGB(
-    //     0.25
-    //   )} 0%, ${this.randomRGB(0.5)} 100%)`;
-    // },
+    randomGradient() {
+      return `radial-gradient( circle, ${this.randomRGB(
+        0.25
+      )} 0%, ${this.randomRGB(0.5)} 100%)`;
+    },
     randomXPos() {
       return Math.ceil(Math.random() * 100) + "%";
     },
@@ -40,7 +40,7 @@ export default {
       return this.min + Math.ceil(Math.random() * (this.max - this.min)) + "%";
     },
     randomSize() {
-      return 5 + Math.ceil(Math.random() * 9) + "px";
+      return 2 + Math.ceil(Math.random() * 6) + "px";
     },
     randomDelay() {
       return Math.ceil(Math.random() * 15) + "s";
@@ -48,16 +48,17 @@ export default {
   },
 
   methods: {
-    // randomColor(min, max) {
-    //   return min + Math.ceil(Math.random() * (max - min));
-    // },
-    // randomRGB(opacity) {
-    //   return `rgba(
-    //     ${this.randomColor(0, 200)},
-    //   ${this.randomColor(204, 244)},
-    //   ${this.randomColor(150, 230)}, ${opacity}
-    //   )`;
-    // },
+    randomColor(min, max) {
+      return min + Math.ceil(Math.random() * (max - min));
+    },
+
+    randomRGB(opacity) {
+      return `rgba(
+        ${this.randomColor(0, 200)}, 
+      ${this.randomColor(204, 244)}, 
+      ${this.randomColor(150, 230)}, ${opacity}
+      )`;
+    },
   },
 };
 </script>
@@ -65,13 +66,10 @@ export default {
 <style lang="scss" scoped>
 .sparkle {
   position: absolute;
-  // width: 2 * 15.5px;
-  // height: 2 * 16.7px;
-  // border: 1px solid (#fff, 0.2);
-  // border-radius: 50%;
-  filter: brightness(0) invert(89%) sepia(23%) saturate(2047%)
-    hue-rotate(107deg) brightness(91%) contrast(91%);
-
+  width: 6px;
+  height: 6px;
+  border: 1px solid (#fff, 0.2);
+  border-radius: 50%;
   opacity: 0;
   animation: float 6s ease-in-out infinite;
 }
@@ -79,11 +77,11 @@ export default {
 @keyframes float {
   0% {
     transform: scale(10%);
-    opacity: 0.03;
+    opacity: 0.3;
   }
   66% {
     transform: scale(100%) translate(0px, -50px);
-    opacity: 0.2;
+    opacity: 1;
   }
   100% {
     transform: translateY(-50px);
